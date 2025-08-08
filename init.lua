298a02ef8f1243a393fd5fcd99b14df3d0182948
syamil <syamil@um.edu.my>
2025-08-09 00:40:42 +0800

    use lazy nvim

--- init.lua
+++ init.lua
@@ -1,24 +1,2 @@
--- bootstrap Lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- vim settings
-vim.opt.relativenumber = true
-vim.opt.number = true
-vim.o.tabstop = 4 
-vim.o.expandtab = true 
-vim.o.softtabstop = 4 
-vim.o.shiftwidth = 4 
-
--- load plugins from lua/plugins
-require("lazy").setup("plugins")
-require("config.keymaps")
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,31 +1,3 @@
--- Keymaps for Neovim
-
-local map = vim.keymap.set
-local opts = { noremap = true, silent = true }
-
--- Space as leader
-vim.g.mapleader = " "
-
--- Save file
-map("n", "<leader>w", ":w<CR>", opts)
-
--- Quit
-map("n", "<leader>q", ":q<CR>", opts)
-
--- Save & quit
-map("n", "<leader>x", ":x<CR>", opts)
-
--- Reload config
-map("n", "<leader>r", ":source $MYVIMRC<CR>", opts)
-
--- Clear search highlight
-map("n", "<leader>h", ":nohlsearch<CR>", opts)
-
--- Telescope (if installed)
-map("n", "<leader>ff", ":Telescope find_files<CR>", opts)
-map("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
-map("n", "<leader>fb", ":Telescope buffers<CR>", opts)
-
--- Open Lazy.nvim
-map("n", "<leader>l", ":Lazy<CR>", opts)
-
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
