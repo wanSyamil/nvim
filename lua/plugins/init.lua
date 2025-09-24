3f2ea54048dfd391f5e34ade3df39ea14e60228f
wanSyamil <syamil.wan46@gmail.com>
2025-09-24 12:25:52 +0800

    fixes and add harpoon2 in lockfile

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,34 +1,7 @@
 return {
   { "folke/lazy.nvim" },
   { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate", -- Automatically runs :TSUpdate when the plugin is installed/updated
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "php", "blade" }, -- Ensure PHP and Blade parsers are installed
-        highlight = {
-          enable = true, -- Enable Treesitter-based highlighting
-          additional_vim_regex_highlighting = false, -- Disable regex highlighting in favor of Treesitter
-        },
-        indent = {
-          enable = true, -- Enable indentation
-        },
-        modules = {}, -- Empty modules, can add specific ones if needed
-        sync_install = true, -- Automatically installs missing parsers
-        ignore_install = {}, -- Ignore specific parsers, leave empty if none
-        auto_install = true, -- Automatically install parsers if missing
-      })
-
-      -- Automatically set PHP syntax for Blade files with inline PHP
-      vim.cmd([[
-        augroup blade_php_filetypes
-          autocmd!
-          autocmd FileType blade,php setlocal syntax=php
-        augroup END
-      ]])
-    end,
-  },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
   {
     "ricardoramirezr/blade-nav.nvim",
     dependencies = {
