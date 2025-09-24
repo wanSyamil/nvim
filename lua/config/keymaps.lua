605027a8791381adc73d3b742c37b0192863c831
wanSyamil <syamil.wan46@gmail.com>
2025-09-24 14:33:01 +0800

    fix harpoon keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,3 +15,22 @@ vim.keymap.set("n", "x", '"_x', { noremap = true, silent = true })
 vim.keymap.set("x", "p", '"_dP', { desc = "[P]ut without yanking" })
 -- vim.keymap.set({ "n", "x" }, "p", '"0p', { noremap = true, silent = true })
 -- vim.keymap.set({ "n", "x" }, "p", '"0p', { noremap = true, silent = true })
+
+-- Harpoon keymaps
+local harpoon = require("harpoon")
+harpoon:setup()
+
+vim.keymap.set("n", "<A-1>", function() harpoon:list():select(1) end, { desc = "Harpoon File 1" })
+vim.keymap.set("n", "<A-2>", function() harpoon:list():select(2) end, { desc = "Harpoon File 2" })
+vim.keymap.set("n", "<A-3>", function() harpoon:list():select(3) end, { desc = "Harpoon File 3" })
+vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end, { desc = "Harpoon File 4" })
+
+vim.keymap.del("n", "<leader>1")
+vim.keymap.del("n", "<leader>2")
+vim.keymap.del("n", "<leader>3")
+vim.keymap.del("n", "<leader>4")
+vim.keymap.del("n", "<leader>5")
+vim.keymap.del("n", "<leader>6")
+vim.keymap.del("n", "<leader>7")
+vim.keymap.del("n", "<leader>8")
+vim.keymap.del("n", "<leader>9")
