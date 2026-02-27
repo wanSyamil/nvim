a1cbed5e6c7dc43eb6d3a7662437cf74ac584c39
Syamil <syamil@um.edu.my>
2026-02-27 10:22:44 +0800

    Important update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,5 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
-  {
     "rose-pine/neovim",
     name = "rose-pine",
     config = function()
---
+++ lua/plugins/visualmulti.lua
@@ -0,0 +1,8 @@
+-- ~/.config/nvim/lua/plugins/visual-multi.lua
+return {
+  "mg979/vim-visual-multi",
+  lazy = false, -- Required: Must be loaded immediately
+  init = function()
+    -- Optional: Configure keybindings here
+  end,
+}
